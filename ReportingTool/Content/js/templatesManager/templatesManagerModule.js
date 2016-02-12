becf73a06c49727e3679db6d35d71c3a1abf7cc5
Nordream <fedorukoy@gmail.com>
2016-02-12 15:10:53 +0200

    Merged with origin

---
+++ ReportingTool/Content/js/templatesManager/templatesManagerModule.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+var templatesManagerModule = angular.module("templatesManagerModule", ['ui.router']);
+
+templatesManagerModule.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    var templatesManager = {
+        name: 'mainView.templatesManager',
+        url: '/templatesManager',
+
+        views: {
+            '': {
+                templateUrl: 'Content/templates/templatesManager/templatesManagerView.html'
+            },
+            'templatesList@mainView.templatesManager': {
+                templateUrl: 'Content/templates/templatesManager/templatesList.html'
+            }
+        }
+    };
+
+   $stateProvider.state(templatesManager);
+}]);
