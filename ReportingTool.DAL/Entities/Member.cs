1fa408b9f9c45f8b194afbdbed9f6d20fc1ff759
amarusiak <andrii.marusiak@gmail.com>
2016-02-22 19:23:57 +0200

    good Team, Member files added

---
+++ ReportingTool.DAL/Entities/Member.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace ReportingTool.DAL.Entities
+{
+    public class Member
+    {
+        public Member()
+        {
+            this.Teams = new HashSet<Team>();
+        }
+
+        public Member(string userName, string fullName)
+        {
+            this.UserName = userName;
+            this.FullName = fullName;
+        }
+
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        [JsonProperty("userName")]
+        [Index(IsUnique = true)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        [JsonProperty("fullName")]
+        public string FullName { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public bool IsActive { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Team> Teams { get; set; }
+    }
+}
