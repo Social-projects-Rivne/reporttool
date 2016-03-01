d1803e1a9eaf429ace1e0e2402921fca75ea5607
Olya <olya.khariv@gmail.com>
2016-03-01 14:10:08 +0200

    add FieldType  table

---
+++ ReportingTool.DAL/Entities/FieldType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportingTool.DAL.Entities
+{
+    public class FieldType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Type { get; set; }
+    }
+}
