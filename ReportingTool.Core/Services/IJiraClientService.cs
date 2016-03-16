875d4ef5cfee9068ea33d13780869105e48d578a
Oleksandr Pastukhov <oleksandr.pastukhov@gmail.com>
2016-03-16 22:13:47 +0200

    Counting members activity, JiraClientService, validation

---
+++ ReportingTool.Core/Services/IJiraClientService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using ReportingTool.DAL.Entities;
+
+namespace ReportingTool.Core.Services
+{
+    public interface IJiraClientService
+    {
+        int GetUserActivity(string userName, string dateFrom, string dateTo);
+        List<Issue> GetIssuesWithWorklogs(string dateFrom, string dateTo);
+    }
+}
--- ReportingTool.DAL/DataAccessLayer/JiraClient.cs
+++ ReportingTool.DAL/DataAccessLayer/JiraClient.cs
@@ -7,11 +7,12 @@ using RestSharp;
 using RestSharp.Deserializers;
 using System.Net;
 using ReportingTool.DAL.Entities;
+using System.Web;
 
 
 namespace ReportingTool.DAL.DataAccessLayer
 {
-    public class JiraClient
+    public class JiraClient : IJiraClient
     {
         private string username;
         private string password;
@@ -76,14 +77,74 @@ namespace ReportingTool.DAL.DataAccessLayer
             return result;
         }
 
-        //public List<JiraUser> GetUsersInGroup(string groupName)
-        //{
-        //    string path = "group?groupname=" + groupName;
-        //    var request = CreateRequest(Method.GET, path);
+        /// <summary>
+        /// Retreiving of issues using one request
+        /// </summary>
+        /// <param name="dateFrom">Lower boundary of time period</param>
+        /// <param name="dateTo">Upper boundary of time period</param>
+        /// <param name="startAt">Start position query</param>
+        /// <returns></returns>
+        public List<Issue> GetIssues(string dateFrom, string dateTo, int startAt)
+        {
+            var curProjectName = HttpContext.Current.Session["projectKey"] as string;
 
-        //    var response = ExecuteRequest(request);
-        //    AssertStatus(response, HttpStatusCode.OK);
-        //    return deserializer.Deserialize<List<JiraUser>>(response);
-        //}
+            var correctedDateFrom = DateTime.Parse(dateFrom).AddDays(-1).ToString("yyyy-MM-dd");
+            var correctedDateTo = DateTime.Parse(dateTo).AddDays(+1).ToString("yyyy-MM-dd");
+
+            string path = "search?jql=updated >" + correctedDateFrom + 
+                " and updated < " + correctedDateTo + 
+                " and project = " + curProjectName + 
+                " and timespent > 0&fields=summary,worklog" +
+                "&startAt=" + startAt + "&maxResults=" + 50;
+            var request = CreateRequest(Method.GET, path);
+
+            var response = ExecuteRequest(request);
+            AssertStatus(response, HttpStatusCode.OK);
+
+            deserializer.RootElement = "issues";
+            return deserializer.Deserialize<List<Issue>>(response);
+        }
+
+        /// <summary>
+        /// Retreiving of all issues considering restrictions of Jira
+        /// </summary>
+        /// <param name="dateFrom">Lower boundary of time period</param>
+        /// <param name="dateTo">Upper boundary of time period</param>
+        /// <returns>List of issues</returns>
+        public List<Issue> GetAllIssues(string dateFrom, string dateTo)
+        {
+            List<Issue> result = new List<Issue>();
+            int count = 0;
+            int cursor = 0;
+            do
+            {
+                var issues = GetIssues(dateFrom, dateTo, cursor);
+                count = issues.Count;
+                foreach (var issue in issues)
+                {
+                    result.Add(issue);
+                }
+                cursor += 50;
+            } while (count >= 50);
+
+            return result;
+        }
+        /// <summary>
+        /// Retreiving of worklog for issue with specific key
+        /// </summary>
+        /// <param name="issueKey">Key of specific issue</param>
+        /// <returns>Worklog of cpecific issue</returns>
+        public  Worklog GetWorklogByIssueKey(string issueKey)
+        {
+            string path = "/issue/" + issueKey + "/worklog";
+            var request = CreateRequest(Method.GET, path);
+
+            var response = ExecuteRequest(request);
+            AssertStatus(response, HttpStatusCode.OK);
+
+            deserializer.RootElement = "issues.fields.worklog";
+            return deserializer.Deserialize<Worklog>(response);
+        }
+
     }
 }
