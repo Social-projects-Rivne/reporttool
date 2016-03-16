ba63fd7e7984e6fa3f5f03964b2b2b3f0cb0bc54
Oleksandr Pastukhov <oleksandr.pastukhov@gmail.com>
2016-03-16 22:33:49 +0200

    IJiraClientService interface small fix

--- ReportingTool.Core/Services/IJiraClientService.cs
+++ ReportingTool.Core/Services/IJiraClientService.cs
@@ -7,6 +7,5 @@ namespace ReportingTool.Core.Services
     public interface IJiraClientService
     {
         int GetUserActivity(string userName, string dateFrom, string dateTo);
-        List<Issue> GetIssuesWithWorklogs(string dateFrom, string dateTo);
     }
 }
