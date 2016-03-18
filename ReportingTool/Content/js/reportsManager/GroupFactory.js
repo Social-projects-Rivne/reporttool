d4057a070b236908e09ebef151a8f2d0ae7487f9
amarusiak <andrii.marusiak@gmail.com>
2016-03-18 18:54:22 +0200

    htmltemplate: GroupFactory

---
+++ ReportingTool/Content/js/reportsManager/GroupFactory.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+//  ---  from TeamFactory ------------------------------------------------------------------   //  debug
+
+reportsManagerModule.
+factory('GroupFactory', ['$http', function ($http) {
+
+    var groupFactory = {
+        GetAllGroups: GetAllGroups,
+        getAllMembers: getAllMembers,
+        updateGroup: updateGroup,
+        deleteGroup: deleteGroup,
+        createGroup: createGroup
+    };
+
+    function GetAllGroups() {
+        //var teams = $http.get("Teams/GetAllTeams");
+        var groups = $scope.groups;
+        return groups;
+    }
+
+    function getAllMembers() {
+        //var teams = $http.get("Teams/GetAllTeams");
+        var members = $scope.reportedMembers;
+        return members;
+    }
+
+    function createGroup(newGroup) {
+        //return $http({
+        //    url: 'Teams/AddNewTeam',
+        //    method: 'POST',
+        //    data: newTeam,
+        //    headers: { 'content-type': 'application/json' }
+        //});
+        var tmpGroup = {};
+        tmpGroup = newGroup;
+        return tmpGroup;
+    }
+
+    function updateGroup(data) {
+        //return $http.put("Groups/EditTeam", data);
+        //  TODO ? - there is no need to update. create-delete would be enough
+    }
+
+    function deleteGroup(id) {
+        //return $http.delete("Teams/DeleteTeam", { params: { id: id } });
+    }
+
+    return groupFactory;
+}]);
+
+//  ---------------------------------------------------------------------------------------------------  //  debug
--- ReportingTool/Content/js/reportsManager/reportGroupController.js
+++ ReportingTool/Content/js/reportsManager/reportGroupController.js
@@ -1,169 +1,218 @@
 ﻿'use strict';
 
-//  ----------------------------------------------------------------------------------------------- //  debug
 reportsManagerModule.controller('reportGroupController',
-    ['$scope', '$state', 'GroupFactory', function ($scope, $state, GroupFactory) {
+    ['$scope', '$state', 'GroupFactory',
+        function ($scope, $state, GroupFactory) {
 
-        //  initial member list for report
-        $scope.reportedMembers = [];
+            //  initial array of members for report
+            $scope.reportedMembers = [];
 
-        // an array of groups in the report
-        $scope.reportedGroups = [];
-
-        $scope.editGroup = {
-            groupID: "0",
-            groupName: "",
-            members: []
-        };
-
-        $scope.selectedMember = {
-            userName: '',
-            fullName: ''
-        };
-
-        $scope.message = "Loading...";
-
-        //$scope.showTeams = true;
-
-        $scope.groups = {};
-        $scope.activeGroup = {};
-
-        //  $scope.validationIsInProgress = true;
-
-        // ----------------------------------------------------------------------------------------
-        GroupFactory.GetAllGroups()
-            .then(groupsSuccess, groupsFail);
-
-            function groupsSuccess(response) {
-                //$scope.groups = response.data;
-                //$scope.validationIsInProgress = false;
-                //$scope.showGroups = true;
-            }
-
-            function groupsFail(response) {
-                //$scope.validationIsInProgress = false;
-                alert("Error: " + response.code + " " + response.statusText);
-            }
-
-        //  --------------------------------------------------------------------------------------
-        var DeleteGroup = null;
-
-        $scope.deleteGroup = function (deletedGroupID) {
-            GroupFactory
-                .deleteGroup(deletedGroupID)
-                .then(delSuccess, delFail);
-        }
-
-        function delSuccess(response) {
-            //if (response.data.Answer == 'Deleted') {
-            //    $state.go('mainView.teamsManager', {}, { reload: true });
-            //}
-        }
-
-        function delFail(response) {
-            console.error("Delete failed!");
-        }
-        //  --------------------------------------------------------------------------------------
-
-        $scope.showGroupMembers = function (selectedGroup) {
-            $scope.activeGroup = selectedGroup;
-        }
-
-        $scope.editGroup = function (updateGroup) {
-            TempObjectFactory.set(updateGroup);
+            // an array of groups in the report
+            $scope.reportedGroups = [];
+            $scope.groups = {};
             $scope.activeGroup = {};
 
-            //  TODO : a view for Group edit ? - maybe add + delete will be enough
-            $state.go('mainView.groupsManager.editGroup');
-        }
-
-       
-
-      
-
-
-
-
-
-
-
-
-
-
-
-        //  ------  get members part of code ---------------------------------------
-        //UserFactory.getJiraUsers().then(getJiraUsersSuccess, getJiraUsersFail);
-        //function getJiraUsersSuccess(response) {
-        //    $scope.jiraUsers = response.data;
-        //}
-        //function getJiraUsersFail(response) {
-        //    console.error("getUsers error!");
-        //};
-
-        //$scope.showEditBlock = true;
-        //  -------------------------------------------------------------------------------------
-
-        // --------------------------------------------------------------------------------------------------------
-        //  DONE : add a member to the group
-        $scope.addMember = function (member) {
-            //  check to avoid member duplication
-            for (var i in $scope.editGroup.members) {
-                if ($scope.editGroup.members[i].userName === member.userName) {
-                    return;
-                }
-            }
-            $scope.editGroup.members.push(member);
-
-            //  remove the added member from initial report list
-            for (var i in $scope.editGroup.members) {
-                if ($scope.editGroup.members[i].userName === userName) {
-                    $scope.reportedMembers.splice(i, 1);
-                    //  $scope.editGroup.members.splice(i, 1);
-                }
-            }
-        }
-
-        // --------------------------------------------------------------------------------------------------------
-        //  DONE : group creation saved
-        $scope.save = function () {
-            GroupFactory.createGroup($scope.editGrup)
-                .then(createSuccess, createFail);
-        }
-
-            //  DONE : create group success
-            function createSuccess(response) {
-                //if (response.data.Answer == 'Created') {
-                //    $state.go('mainView.teamsManager', {}, { reload: true });
-                //}
-
-                $state.go('mainView.reportsManager.reportsConditions.reportDraft', {}, { reload: true });
-            }
-
-            //  DONE : create group fail
-            function createFail(response) {
-                console.error('create group fail!');
-            }
-
-        // --------------------------------------------------------------------------------------------------------
-        //  DONE : group creation cancelled
-        $scope.cancel = function () {
+            //  a work template for group manipulation
             $scope.editGroup = {
-                groupID: "",
+                //groupID: "0",
                 groupName: "",
                 members: []
             };
-            //$state.go('mainView.teamsManager');
-            $state.go('mainView.reportsManager.reportsConditions.reportDraft');
-        }
 
-        // --------------------------------------------------------------------------------------------------------
-        //  DONE : remove a member from the group
-        $scope.removeMember = function (userName) {
-            for (var i in $scope.editGroup.members) {
-                if ($scope.editGroup.members[i].userName === userName) {
-                    $scope.editGroup.members.splice(i, 1);
+            //  a work template for member manipulation
+            $scope.selectedMember = {
+                userName: '',
+                fullName: ''
+            };
+
+            //  $scope.message = "Loading...";
+            //  $scope.showTeams = true;
+            //  $scope.validationIsInProgress = true;
+
+            $scope.tempReport = TempObjectFactory.get();
+            TempObjectFactory.set({});
+
+            // === from teamsManagerController ========================
+            GroupFactory.GetAllGroups()
+                .then(groupsSuccess, groupsFail);
+
+                function groupsSuccess(response) {
+                    //$scope.groups = response.data;
+                    //$scope.validationIsInProgress = false;
+                    //$scope.showGroups = true;
+                }
+                function groupsFail(response) {
+                    //$scope.validationIsInProgress = false;
+                    alert("Error: " + response.code + " " + response.statusText);
+                }
+            //  --------------------------------------------------------------------------------------
+            var DeleteGroup = null;
+
+            $scope.deleteGroup = function (deletedGroupID) {
+                GroupFactory
+                    .deleteGroup(deletedGroupID)
+                    .then(delSuccess, delFail);
+            }
+
+                function delSuccess(response) {
+                    //if (response.data.Answer == 'Deleted') {
+                    //    $state.go('mainView.teamsManager', {}, { reload: true });
+                    //}
+                }
+                function delFail(response) {
+                    console.error("Delete failed!");
+                }
+            //  --------------------------------------------------------------------------------------
+            $scope.showGroupMembers = function (selectedGroup) {
+                $scope.activeGroup = selectedGroup;
+            }
+
+            $scope.editGroup = function (updateGroup) {
+                TempObjectFactory.set(updateGroup);
+                $scope.activeGroup = {};
+
+                //  TODO : a view for Group edit ? - maybe add + delete will be enough
+                $state.go('mainView.groupsManager.editGroup');
+            }
+            // ======================================================
+
+            // === from teamsEditController =============================
+            $scope.groupUsers = [{
+                loginName: 'Loading...',
+                fullName: 'Loading...'
+            }];
+
+            $scope.editGroup = TempObjectFactory.get();
+
+            $scope.selectedMember = {
+                userName: '',
+                fullName: ''
+            }
+            //  --------------------------------------------------------------------------------------
+            GroupFactory.getAllMembers()
+                .then(getAllMembersSuccess, getAllMembersFail);
+
+                function getAllMembersSuccess(response) {
+                    $scope.groupUsers = response.data;
+                }
+                function $scope.groupUsersFail(response) {
+                    console.error("getUsers error!");
+                };
+            //  --------------------------------------------------------------------------------------
+            $scope.message = "Loading...";
+            $scope.showEditBlock = true;
+
+            $scope.addMember = function (member) {
+                //  check to avoid member duplication
+                for (var i in $scope.editGroup.members) {
+                    if ($scope.editGroup.members[i].userName === member.userName) {
+                        return;
+                    }
+                }
+                $scope.editGroup.members.push(member);
+
+                //  remove the added member from initial report list
+                for (var i in $scope.reportedMembers) {
+                    if ($scope.reportedMembers[i].userName === member.userName) {
+                        $scope.reportedMembers.splice(i, 1);
+                        //  $scope.editGroup.members.splice(i, 1);
+                    }
                 }
             }
-        }
 
-    }]);
+            $scope.removeMember = function (userName) {
+                console.log('del member ' + userName);
+
+                for (var i in $scope.editGroup.members) {
+                    if ($scope.editGroup.members[i].userName === userName) {
+                        $scope.editGroup.members.splice(i, 1);
+                    }
+                }
+            }
+
+            $scope.save = function () {
+                GroupFactory.updateTeam($scope.editTeam)
+                    .then(updateSuccess, updateFail);
+
+                TempObjectFactory.set({});
+            }
+                function updateSuccess(response) {
+                    //$state.go('mainView.teamsManager', {}, { reload: true });
+                    $state.go('mainView.reportsManager.reportsConditions.reportDraft', {}, { reload: true });
+                }
+                function updateFail(response) {
+                    console.error("error during saving edited team");
+                }
+
+                $scope.cancel = function () {
+                    TempObjectFactory.set({});
+
+                    //$state.go('mainView.teamsManager');
+                    $state.go('mainView.reportsManager.reportsConditions.reportDraft', {}, { reload: true });
+            }
+          
+            // ======================================================
+
+            // ======================================================
+
+            //  === from NewTeamController ============================
+            //  $scope.showEditBlock = true;
+            //  DONE : add a member to the group
+            $scope.addMember = function (member) {
+                //  check to avoid member duplication
+                for (var i in $scope.editGroup.members) {
+                    if ($scope.editGroup.members[i].userName === member.userName) {
+                        return;
+                    }
+                }
+                $scope.editGroup.members.push(member);
+
+                //  remove the added member from initial report list
+                for (var i in $scope.reportedMembers) {
+                    if ($scope.reportedMembers[i].userName === member.userName) {
+                        $scope.reportedMembers.splice(i, 1);
+                        //  $scope.editGroup.members.splice(i, 1);
+                    }
+                }
+            }
+            //  --------------------------------------------------------------------------------------
+            //  DONE : group creation saved
+            $scope.save = function () {
+                GroupFactory.createGroup($scope.editGrup)
+                    .then(createSuccess, createFail);
+            }
+                //  DONE : create group success
+                function createSuccess(response) {
+                    //if (response.data.Answer == 'Created') {
+                    //    $state.go('mainView.teamsManager', {}, { reload: true });
+                    //}
+
+                    $state.go('mainView.reportsManager.reportsConditions.reportDraft', {}, { reload: true });
+                }
+                //  DONE : create group fail
+                function createFail(response) {
+                    console.error('create group fail!');
+                }
+            //  --------------------------------------------------------------------------------------
+            //  DONE : group creation cancelled
+            $scope.cancel = function () {
+                $scope.editGroup = {
+                    //groupID: "",
+                    groupName: "",
+                    members: []
+                };
+                //$state.go('mainView.teamsManager');
+                $state.go('mainView.reportsManager.reportsConditions.reportDraft');
+            }
+            //  --------------------------------------------------------------------------------------
+            //  DONE : remove a member from the group
+            $scope.removeMember = function (userName) {
+                for (var i in $scope.editGroup.members) {
+                    if ($scope.editGroup.members[i].userName === userName) {
+                        $scope.editGroup.members.splice(i, 1);
+                    }
+                }
+            }
+            //  =====================================================
+
+        }]);
